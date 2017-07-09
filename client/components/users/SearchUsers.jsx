707bef32227882a9c2e73ce5cb27aedb8c13a572
Andela Developer <mayowa.makinde@andela.com>
2017-07-09 23:05:25 +0100

    feature(search): search users
    [Finishes #148551771]

---
+++ client/components/users/SearchUsers.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+export default class SearchUsers extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchQuery: ''
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(event) {
+    this.setState({
+      searchQuery: event.target.value
+    });
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    this.props.searchUserDb(this.state.searchQuery);
+  }
+
+  render() {
+    return (
+      <form className="input-field user" onSubmit={this.onSubmit}>
+        <div >
+          <input
+            id="search"
+            type="search"
+            placeholder="Search Users"
+            onChange={this.onChange}
+          />
+          <label className="label-icon" htmlFor="search">
+            <i className="material-icons">search</i>
+          </label>
+        </div>
+      </form>
+    );
+  }
+}
+
+SearchUsers.propTypes = {
+  searchUserDb: PropTypes.func.isRequired
+};
