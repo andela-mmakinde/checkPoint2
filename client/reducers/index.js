36851fe2f525d89c0e9ba9415b004a8b5fd8b153
Andela Developer <mayowa.makinde@andela.com>
2017-07-14 11:52:45 +0100

    Clean up staging branch

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,12 +2,10 @@ import { combineReducers } from 'redux';
 import documents from './documentReducers';
 import auth from './authReducers';
 import user from './userReducers';
-import apiCall from './apiCall';
 
 const rootReducer = combineReducers({
   documents,
   auth,
-  apiCall,
   user
 });
 
