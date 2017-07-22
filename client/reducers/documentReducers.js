4499cc1205730c665c1d1c06fd33110cc40c5829
Andela Developer <mayowa.makinde@andela.com>
2017-07-22 14:33:21 +0100

    add testes for reducers

--- client/reducers/documentReducers.js
+++ client/reducers/documentReducers.js
@@ -1,17 +1,19 @@
 import * as actionTypes from '../actions/actionType';
 
-export default (state = [], action) => {
+const initialState = {
+  documentList: {},
+  pagination: {}
+};
+export default (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.GET_DOCUMENT_SUCCESS:
-      return { ...state, ...action.documents };
+      return { ...state, documentList: action.documents.documents, pagination: action.documents.pagination };
     case actionTypes.GET_USER_DOCUMENT_SUCCESS:
-      return { ...state, ...action.documents };
-    case actionTypes.DELETE_DOCUMENT_SUCCESS:
-      return action.successMessage.data.message;
+      return { ...state, documentList: action.documents.documents, pagination: action.documents.pagination };
     case actionTypes.SEARCH_DOCUMENT_SUCCESS:
-      return { ...state, ...action.documents };
+      return { ...state, documentList: action.documents.documents, pagination: action.documents.pagination };
     case actionTypes.SEARCH_DOCUMENTBYID_SUCCESS:
-      return { ...state, ...action.document };
+      return { ...state, documentList: action.document.data };
     default:
       return state;
   }
