6f7de21f82b7852bad217ae83ddb8ff222e31291
Andela Developer <mayowa.makinde@andela.com>
2017-07-26 16:35:25 +0100

    chore(e2e):add end-to-end tests
    [Finishes #148899667]

--- client/test/e2e/admin.js
+++ client/test/e2e/admin.js
@@ -1,37 +1,37 @@
 module.exports = {
   'Admin should be able to view a list of all users': browser =>
     browser
-      .url('http://localhost:3000/login')
+      .url('http://localhost:5000/login')
       .waitForElementVisible('body', 5000)
       .setValue('input[name=email]', 'mayowa@andela.com')
       .setValue('input[name=password]', 'andela')
       .click('.btn-large')
       .pause(2000)
-      .assert.urlEquals('http://localhost:3000/document')
+      .assert.urlEquals('http://localhost:5000/document')
 
       .click('.users')
       .waitForElementVisible('body', 5000)
       .pause(2000)
-      .assert.urlEquals('http://localhost:3000/user'),
+      .assert.urlEquals('http://localhost:5000/user'),
   'Admin should be able to delete a user': browser =>
     browser
-      .url('http://localhost:3000/user')
+      .url('http://localhost:5000/user')
       .waitForElementVisible('body', 5000)
-      .click('.deleteModalTrigger')
+      .click('#deleteModalTrigger')
       .pause(2000)
       .click('.delete')
-      .waitForElementVisible('.toast', 5000)
+      .waitForElementVisible('.toast', 10000)
       .assert.containsText('.toast', 'User deleted'),
   'Admin should be able to search users': browser =>
     browser
-      .url('http://localhost:3000/user')
-      .waitForElementVisible('body', 3000)
+      .url('http://localhost:5000/user')
+      .waitForElementVisible('body', 5000)
       .clearValue('input[name=userSearch]')
       .setValue('input[name=userSearch]', 'ama')
       .keys(browser.Keys.ENTER)
       .pause(5000)
       .waitForElementVisible('.userTable', 2000)
       .pause(2000)
-      .assert.containsText('.userEmail', 'ama@la.com')
+      .assert.containsText('#userEmail', 'ama@la.com')
       .end()
 };
--- client/test/e2e/documents.js
+++ client/test/e2e/documents.js
@@ -1,85 +1,84 @@
 const faker = require('faker');
 
 const title = faker.lorem.words(2);
-const content = faker.lorem.paragraphs();
+const content = faker.lorem.paragraphs(2);
 const email = faker.internet.email();
 const password = faker.internet.password();
 module.exports = {
-  'User should be able to create document successfully': browser =>
+  'User should receive an error message if any field is left unfilled': browser =>
     browser
-     .url('http://localhost:3000/signup')
+     .url('http://localhost:5000/signup')
       .waitForElementVisible('body', 5000)
       .setValue('input[name=fullName]', 'Mayowa Makinde')
       .setValue('input[name=email]', email)
       .setValue('input[name=password]', password)
       .setValue('input[name=confirmPassword]', password)
       .click('.btn-large')
-      .pause(2000)
-      .assert.urlEquals('http://localhost:3000/document')
+      .pause(1000)
+      .assert.urlEquals('http://localhost:5000/document')
       .click('.btn-large')
-      .pause(5000)
+      .pause(1000)
+      .assert.urlEquals('http://localhost:5000/create')
       .setValue('input[name=title]', title)
-      // .click('select option[value="public"]')
       .click('.DraftEditor-editorContainer')
       .click('div.public-DraftStyleDefault-block')
-      .setValue('.public-DraftEditor-content', content),
-      // .click('.submit')
-      // .pause(1000)
-      // .waitForElementVisible('.toast', 5000)
-      // .assert.containsText('.toast', 'Document Created')
-      // .waitForElementVisible('.document-list-view', 5000)
-      // .pause(5000)
-      // .assert.containsText('.scrollable > div > a', title)
-
+      .setValue('.public-DraftEditor-content', content)
+      .click('.submit')
+      .pause(1000)
+      .waitForElementVisible('.toast', 5000)
+      .assert.containsText('.toast', 'Please fill out all fields'),
+  'User should be able to create document successfully': browser =>
+    browser
+      .assert.urlEquals('http://localhost:5000/create')
+      .pause(1000)
+      .click('select option[value="Role"]')
+      .click('.submit')
+      .pause(1000)
+      .assert.urlEquals('http://localhost:5000/docs'),
   'User should be able to retrieve all documents he/she created': browser =>
     browser
-     .url('http://localhost:3000/document')
+     .url('http://localhost:5000/document')
       .waitForElementVisible('body', 5000)
       .click('.dropdown-button')
       .pause(1000)
       .click('#myDocs')
       .pause(2000)
-      .assert.urlEquals('http://localhost:3000/docs'),
+      .assert.urlEquals('http://localhost:5000/docs'),
   'User should be able to search documents': browser =>
     browser
-      .url('http://localhost:3000/document')
-      .waitForElementVisible('body', 3000)
+      .url('http://localhost:5000/document')
+      .waitForElementVisible('body', 5000)
       .clearValue('input[name=search]')
-      .setValue('input[name=search]', 'new')
+      .setValue('input[name=search]', 'Mayowa')
       .keys(browser.Keys.ENTER)
       .pause(5000)
-      .waitForElementVisible('.documentCard', 2000)
+      .waitForElementVisible('.documentCard', 5000),
+  'User should be able to edit a document successfully': browser =>
+    browser
+     .url('http://localhost:5000/docs')
+      .waitForElementVisible('body', 5000)
       .pause(2000)
-      .assert.containsText('.title', 'New 1'),
+      .click('.edit')
+      .pause(2000)
+      .clearValue('input[name=title]')
+      .setValue('input[name=title]', 'new title')
+      .click('select option[value="Public"]')
+      .click('.DraftEditor-editorContainer')
+      .click('div.public-DraftStyleDefault-block')
+      .clearValue('.public-DraftEditor-content')
+      .setValue('.public-DraftEditor-content', content)
+      .click('.submit')
+      .pause(5000)
+      .assert.urlEquals('http://localhost:5000/docs'),
   'User should be able to view document details': browser =>
     browser
-      .url('http://localhost:3000/document')
-      .waitForElementVisible('body', 3000)
-      .click('.viewDocument')
+      .url('http://localhost:5000/document')
+      .waitForElementVisible('body', 5000)
+      .click('a.documentView')
       .pause(2000)
-      .waitForElementVisible('.modal', 2000)
+      .waitForElementVisible('.modal', 10000)
       .pause(2000)
-      .assert.containsText('.center', 'My document')
-  // 'User should be able to edit a document successfully': browser =>
-  //   browser
-  //    .url('http://localhost:3000/docs')
-  //     .waitForElementVisible('body', 5000)
-  //     .click('.edit')
-  //     .pause(2000)
-  //     .clearValue('input[name=title]')
-  //     .setValue('input[name=title]', 'new title')
-  //     // .click('select option[value="public"]')
-  //     .click('.DraftEditor-editorContainer')
-  //     .click('div.public-DraftStyleDefault-block')
-  //     .clearValue('.public-DraftEditor-content')
-  //     .setValue('.public-DraftEditor-content', content)
-      // .click('.submit')
-      // .pause(1000)
-      // .waitForElementVisible('.toast', 5000)
-      // .assert.containsText('.toast', 'Document Created')
-      // .waitForElementVisible('.document-list-view', 5000)
-      // .pause(5000)
-      // .assert.containsText('.scrollable > div > a', title),
+      .assert.containsText('.center', 'new title')
       .end()
 };
 
