a351548e471ef33718564fee829920bdda99fe17
Andela Developer <mayowa.makinde@andela.com>
2017-07-27 08:45:16 +0100

    made date on footer dynamic

--- client/components/includes/Footer.jsx
+++ client/components/includes/Footer.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 
-const Footer = () => (
-  <footer className="page-footer indigo">
-    <div className="footer-copyright">
-      <div className="container center">
-        <a
-          className="orange-text text-lighten-4"
-          href="https://www.linkedin.com/in/makinde-mayowa-650b148b/"
-        >
-          Makinde Mayowa&emsp;&emsp;
-        </a>
-        © 2017
+const Footer = () => {
+  const date = new Date().getFullYear();
+  return (
+    <footer className="page-footer indigo">
+      <div className="footer-copyright">
+        <div className="container center">
+          <a
+            className="orange-text text-lighten-4"
+            href="https://www.linkedin.com/in/makinde-mayowa-650b148b/"
+          >
+            Makinde Mayowa&emsp;&emsp;
+          </a>
+          {date}
+        </div>
       </div>
-    </div>
-  </footer>
-);
+    </footer>
+  );
+};
 export default Footer;
